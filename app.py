984d0f409e8f489dcb840d02927c4b93674afb87
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-06-08 08:54:21 +0530

    "improved gitignore"

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.get('/alert', response_class=HTMLResponse)
 def read_root():
-    return '<script>alert("This is JS")</script>'
+    return '<script>alert("This is JavaScript")</script>'
 
 @app.get('/amazon', response_class=RedirectResponse)
 def redirect():
